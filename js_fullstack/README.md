f5b5e93304a5d9ac857bd1c475e76d030860ffb7
coderdNoBB <67186168+coderdNoBB@users.noreply.github.com>
2023-09-03 19:46:34 +0800

    Added Signin function more completely.

--- js_fullstack/README.md
+++ js_fullstack/README.md
@@ -1,1 +1,8 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+# NextJs + Next-Auth + TS + TypeORM + Postgres 
+
+Basically you can run the docker compose on local and start development environment for a new project or learning propose quickly.
+
+Start js_fullstack 
+```
+npm run dev
+```
--- js_fullstack/pages/_app.tsx
+++ js_fullstack/pages/_app.tsx
@@ -1,16 +1,18 @@
 import { SessionProvider } from "next-auth/react"
 import '../styles/globals.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-
 import type { AppProps } from "next/app"
 import type { Session } from "next-auth"
-
+import { AppDataSource } from "../src/datasource"
 // Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
+
+
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps<{ session: Session }>) {
+
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
