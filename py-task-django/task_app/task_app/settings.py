f8ba7c1a90e8f21cfc690e1d78a4ce659b61c885
coderdNoBB <67186168+coderdNoBB@users.noreply.github.com>
2023-09-25 22:57:33 +0800

    Added S ETL task to registered task and added to PERIODIC TASKS

--- py-task-django/task_app/task_app/settings.py
+++ py-task-django/task_app/task_app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_celery_results',
+    'django_celery_beat',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CELERY
+# CELERY_BROKER_URL = 'redis://redis:6380'
+CELERY_BROKER_URL = 'redis://localhost:6380'
+CELERY_RESULT_BACKEND = 'django-db'
+CELERY_CACHE_BACKEND = 'django-cache'
